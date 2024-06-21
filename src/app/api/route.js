3f7945dd080ac655c39203e81a02983588482202
Andrets <dmitriysurkov2019@gmail.com>
2024-06-22 00:16:52 +0500

    change route.js

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,5 +1,29 @@
 'use server'
+import { useRouter } from 'next/router'
 import { getSubtitles } from 'youtube-captions-scraper'
+
+export async function generateStaticParams() {
+  const dynamicRoutes = [{dynamic: '/api'}]
+  return dynamicRoutes.map(route => ({
+    dynamic: route.dynamic
+  }))
+}
+
+export default function DynamicPage({params}) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+  return <div>Dynamic Page: {params.dynamic}</div>
+}
+
+export async function getStaticProps({params}) {
+  return {
+    props: {
+      dynamic: params.dynamic
+    }
+  }
+}
 
 export async function GET(req, res) {
   const { searchParams } = new URL(req.url) 
