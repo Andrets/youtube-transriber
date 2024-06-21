f2428675d6f13eb422a64016fef767fa0b14e52c
Andrets <dmitriysurkov2019@gmail.com>
2024-06-22 00:13:38 +0500

    change next.config v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	exportPathMap: async (defaultPathMap, {
-		dev, dir, outDir, distDir, buildId
-	}) => {
-		const pathMap = {
-			...defaultPathMap,
-			'/': { page: '/' }
-		}
-		delete pathMap['/api'];
-
-		return pathMap
-	}
 };
 
 export default nextConfig;
