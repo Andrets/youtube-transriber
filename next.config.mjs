a496b7f9bbfbac419ed994887463204e61121c77
Andrets <dmitriysurkov2019@gmail.com>
2024-06-22 00:50:22 +0500

    fix customer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+	swcMinify: false // disable swc
 };
 
 export default nextConfig;
--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,29 +1,6 @@
 'use server'
-import { usePathname } from 'next/navigation'
 import { getSubtitles } from 'youtube-captions-scraper'
 
-export async function generateStaticParams() {
-  const dynamicRoutes = [{dynamic: '/api'}]
-  return dynamicRoutes.map(route => ({
-    dynamic: route.dynamic
-  }))
-}
-
-export default async function DynamicPage({params}) {
-  const router = usePathname()
-  if (router === '/api') {
-    return <div>Loading...</div>
-  }
-  return <div>Dynamic Page: {params.dynamic}</div>
-}
-
-export async function getStaticProps({params}) {
-  return {
-    props: {
-      dynamic: params.dynamic
-    }
-  }
-}
 
 export async function GET(req, res) {
   const { searchParams } = new URL(req.url) 
